de3fcf5809c9dd9563710d97cb1a29434adfda58
Michael <cmichael40@yahoo.com>
2013-04-11 21:41:10 -0400

    Initial Commit
    Nothing done yet just setting up the workspace

---
+++ Routes/src/minny/routes/Routes.java
@@ -0,0 +1,15 @@
+package minny.routes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Routes extends JavaPlugin{
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
